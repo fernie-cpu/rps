a203995e91fc9cfdc0a197582a622832c87788c6
Fernando Vasconcelos <76075623+fernie-cpu@users.noreply.github.com>
2024-04-11 23:20:16 -0300

    create computer's choice function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('sup')
+const rpsArray = ['rock', 'paper', 'scissors'];
+
+const getComputerChoice = () => {
+    for (let i = 0; i < rpsArray.length; i++) {
+        console.log(rpsArray[i]);
+    }
+}
+
+getComputerChoice();
