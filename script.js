5a9384bac501c0cdcccfe631ad19a7588db0ff1d
Fernando Vasconcelos <76075623+fernie-cpu@users.noreply.github.com>
2024-04-15 19:55:50 -0300

    return computerChoice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const rpsArray = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
     for (let i = 0; i < rpsArray.length; i++) {
-        // console.log(rpsArray[i]);
+        return rpsArray; 
     }
 }
 
