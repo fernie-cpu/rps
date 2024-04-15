ddad700fe52f1ce7f320bca509782f954ffbf5f9
Fernando Vasconcelos <76075623+fernie-cpu@users.noreply.github.com>
2024-04-15 19:55:00 -0300

    create playRound function and conditions

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ const rpsArray = ['rock', 'paper', 'scissors'];
 
 const getComputerChoice = () => {
     for (let i = 0; i < rpsArray.length; i++) {
-        console.log(rpsArray[i]);
+        // console.log(rpsArray[i]);
     }
 }
 
-getComputerChoice();
+const playRound = (playerSelection, computerSelection) => {
+
+    if (computerSelection === "rock" && playerSelection === "paper") {
+        return "You win! Paper beats Rock";
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return "You win! Scissors beats Paper";
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return "You win! Rock beats Scissors";
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You lose! Rock beats Scissors";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "You lose! Paper beats Rock";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "You lose! Scissors beater Paper"
+    } else if (computerSelection === playerSelection) {
+        return "It's a tie."
+    }
+}
+
+const playerSelection = prompt("Enter your choice:");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
