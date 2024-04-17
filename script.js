a3b3adea2a3da7306b11f4a226999eccc325496d
Fernando Vasconcelos <76075623+fernie-cpu@users.noreply.github.com>
2024-04-16 21:10:56 -0300

    edit on comment

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const rpsArray = ['rock', 'paper', 'scissors'];
+/*
+As I forgot to commit due to several attemps on this, I'll leave the changes as a comment, for this time:
+- create variable to set initial score
+- edit playRound to increment score value
+
+- create a winGame function to return the winner
+- edit playGame function to create the for loop that should work :(
+*/
+
+let playerScore = 0;
+let computerScore = 0;
 
 const getComputerChoice = () => {
+    const rpsArray = ['rock', 'paper', 'scissors'];
     // Alternative approach for getting a random item from array
 
     const random = Math.floor(Math.random() * rpsArray.length);
@@ -13,32 +24,51 @@ const getComputerChoice = () => {
 const playRound = (playerSelection, computerSelection) => {
 
     if (computerSelection === "rock" && playerSelection === "paper") {
+        playerScore += 1;
         return "You win! Paper beats Rock";
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        playerScore += 1;
         return "You win! Scissors beats Paper";
     } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        playerScore += 1;
         return "You win! Rock beats Scissors";
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        computerScore += 1;
         return "You lose! Rock beats Scissors";
     } else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore += 1;
         return "You lose! Paper beats Rock";
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore += 1;
         return "You lose! Scissors beats Paper"
     } else if (computerSelection === playerSelection) {
         return "It's a tie."
     }
+    
 }
 
-const playerSelection = prompt("Enter your choice:");
-const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+// console.log(computerSelection);
+const winGame = () => {
+    if (playerScore === 5) {
+        return "You win!"
+    } else if (computerScore === 5) {
+        return "Computer wins!"
+    }
+}
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(computerSelection);
 
-// const playGame = () => {
-//     for (let i = 0; i < 5; i++) {
-//         return playRound();
-//     }
-// }
+const playGame = () => {
+    let playerSelection = prompt("Enter your choice:").toLowerCase();
+    let computerSelection = getComputerChoice();
+    for (let i = 0; i < 5; i++) {
+        if (playerScore < 5 || computerScore < 5) {
+            return playRound(playerSelection, computerSelection);
+        } else {
+            return winGame();
+        }
+    }
+    console.log(winGame());
+ }
 
-// console.log(playGame());
+console.log(playGame());
